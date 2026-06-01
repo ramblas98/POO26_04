ac8d39465190301e667693076d19d3144e4f8283
Hp <Hp@Ester>
2026-06-01 18:03:33 -0300

    version 2

---
+++ EJERCICIO_1/src/ejercicio_1/Rectangualo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_1;
+
+public class Rectangualo {
+    private double base;
+    private double altura;
+
+    public Rectangualo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    public double area(){
+        
+    }
+}
+
+
