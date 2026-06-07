4e0d6f99cf52df04c140cc2aa13abd1547447945
alexisC <vancolialexis9@gmail.com>
2026-06-07 19:53:29 -0300

    ejercicio 5 1.3.2

--- TP_4/src/ejercicio5/main.java
+++ TP_4/src/ejercicio5/main.java
@@ -4,17 +4,24 @@ public class main {
     public static void main(String[] args){
         Deposito d1 = new Deposito();
 
-        Locomotora l1 = new Locomotora();
+        //Locomotora l1 = new Locomotora();
         Locomotora l2 = new Locomotora(2000,4000,8000);
 
         Formacion f1 = new Formacion(231);
         Formacion f2 = new Formacion(2144);
 
         f1.agregarLocomotora(l2);
-        f2.agregarLocomotora(l1);
+        //f2.agregarLocomotora(l1);
 
         d1.agregarFormacion(f1);
         d1.agregarFormacion(f2);
 
+        Locomotora sl1 = new Locomotora();
+        Locomotora sl2 = new Locomotora();
+        Locomotora sl3 = new Locomotora();
+
+        d1.agregarLocomotoraSuelta(sl1);
+        d1.agregarLocomotoraSuelta(sl2);
+        d1.agregarLocomotoraSuelta(sl3);
     }
 }
